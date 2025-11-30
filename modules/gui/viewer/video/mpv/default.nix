9aa0c7179828c2245b99925b735d0ace7b1a09a4
Falkgaard <falkgaard@gmail.com>
2025-11-30 21:19:15 +0100

    Some more mpv configuration

--- modules/gui/viewer/video/mpv/default.nix
+++ modules/gui/viewer/video/mpv/default.nix
@@ -3,21 +3,21 @@
    # TODO: Svp package?
    programs.mpv = {
       enable  = true;
-      # package = pkgs.mpv-unwrapped.wrapper {
-      #    mpv = pkgs.mpv-unwrapped.override {
-      #       vapoursynthSupport = true;
-      #    };
-      #    youtubeSupport = true;
-      # };
+      package = pkgs.mpv-unwrapped.wrapper {
+         mpv = pkgs.mpv-unwrapped.override {
+            vapoursynthSupport = true;
+         };
+         youtubeSupport = true;
+      };
       bindings = {
          # TODO
       };
       config = {
          # TODO
-         # profile       = "gpu-hq";
-         # force-window  = true;
-         # ytdl-format   = "bestvideo+bestaudio";
-         # cache-default = 4000000;
+         profile       = "gpu-hq";
+         force-window  = true;
+         ytdl-format   = "bestvideo+bestaudio";
+         cache-default = 4000000;
       };
       defaultProfiles = [ "gpu-hq" ];
       extraInput = '''';
