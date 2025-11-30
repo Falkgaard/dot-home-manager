c2fa9b873b03e01b148593bf317699f5fcdfc08d
Falkgaard <falkgaard@gmail.com>
2025-11-30 19:45:27 +0100

    Adding mpv as a media player

---
+++ modules/gui/viewer/video/mpv/default.nix
@@ -0,0 +1,26 @@
+{ pkgs, config, ... }:
+{
+   # TODO: Svp package?
+   programs.mpv = {
+      enable  = true;
+      # package = pkgs.mpv-unwrapped.wrapper {
+      #    mpv = pkgs.mpv-unwrapped.override {
+      #       vapoursynthSupport = true;
+      #    };
+      #    youtubeSupport = true;
+      # };
+      bindings = {
+         # TODO
+      };
+      config = {
+         # TODO
+         # profile       = "gpu-hq";
+         # force-window  = true;
+         # ytdl-format   = "bestvideo+bestaudio";
+         # cache-default = 4000000;
+      };
+      defaultProfiles = [ "gpu-hq" ];
+      extraInput = '''';
+   };
+}
+
